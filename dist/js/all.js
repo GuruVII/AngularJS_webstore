dc69879513ac64b8d402128bd3ae1fae52be7be8
Jure Jeglič <guruvii@gmail.com>
2016-05-31 17:04:15 +0200

    testing different carousel code

--- dist/js/all.js
+++ dist/js/all.js
@@ -90,20 +90,26 @@ app.factory('categoryFactory', function ($http, $q) {
 });
 app.controller('carouselController', ["$scope", "carouselFactory", function($scope, carouselFactory){
 
- $scope.interval = 3000;
+  $scope.interval = 3000;
   $scope.noWrapSlides = false;
   $scope.activeSlide = 0;
   $scope.slides = carouselFactory.query();
-	
-/*	$scope.slides.forEach(function (currentValue){
-		if ((currentValue.id % 4) == 0 
-		){
-		$scope.selectedSlides.push(currentValue)}
+
+//$scope.selectedSlides = [{image:"http://lorempixel.com/300/300/people/1", name:"potato"}]
+
+$scope.slides.forEach(function (currentValue){
+	console.log("potato")
+		//if ((currentValue.id % 4) == 0 
+		//){
+		//}
+		console.log(currentValue);
+		$scope.selectedSlides.push(currentValue);
+		console.log($scope.selectedSlides);
 	});
-*/
-	
 
 
+
+ 
 
         }]);
 app.factory('carouselFactory', function($resource) {
--- src/web/home/carouselController.js
+++ src/web/home/carouselController.js
@@ -1,18 +1,24 @@
 app.controller('carouselController', ["$scope", "carouselFactory", function($scope, carouselFactory){
 
- $scope.interval = 3000;
+  $scope.interval = 3000;
   $scope.noWrapSlides = false;
   $scope.activeSlide = 0;
   $scope.slides = carouselFactory.query();
-	
-/*	$scope.slides.forEach(function (currentValue){
-		if ((currentValue.id % 4) == 0 
-		){
-		$scope.selectedSlides.push(currentValue)}
+
+//$scope.selectedSlides = [{image:"http://lorempixel.com/300/300/people/1", name:"potato"}]
+
+$scope.slides.forEach(function (currentValue){
+	console.log("potato")
+		//if ((currentValue.id % 4) == 0 
+		//){
+		//}
+		console.log(currentValue);
+		$scope.selectedSlides.push(currentValue);
+		console.log($scope.selectedSlides);
 	});
-*/
-	
 
 
+
+ 
 
         }]);
